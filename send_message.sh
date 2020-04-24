e2fc26227664b117c41b9102789256fe5f1fa133
Eric Parker <eparker71@gmail.com>
2020-04-24 12:18:54 -0400

    remove send script

--- send_message.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /Users/ericparker/OneDrive/projects/crypto
-source /Users/ericparker/OneDrive/projects/crypto/bin/activate
-/Users/ericparker/OneDrive/projects/crypto/bin/python message.py 
-
